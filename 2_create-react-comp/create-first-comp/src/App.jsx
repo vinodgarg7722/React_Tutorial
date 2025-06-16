3a05acd4809f3a50ebf561ca07fda59f23c3088d
vinod garg <vinodgarg070702@gmail.com>
2025-06-16 16:42:18 +0530

    Module_2

--- 2_create-react-comp/create-first-comp/src/App.jsx
+++ 2_create-react-comp/create-first-comp/src/App.jsx
@@ -1,13 +1,19 @@
 import KgButton from "./KgButton"; // Capitalized import
 import Hello from "./Hello";
+import Random from "./Random";
 
 
 function App() {
   return (
     <div>
       <h1>This is the best React Course</h1>
-      <button>Subscribe</button>
+      {/* <button>Subscribe</button> */}
       <Hello></Hello>
+      <Random></Random>
+      <Random></Random>
+      <Random></Random>
+      <Random></Random>
+      <Random></Random>
     </div>
   );
 }
